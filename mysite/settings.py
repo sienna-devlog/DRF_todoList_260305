f8b7d4382bb0e53ce8bab5abc7f42b3414ea0fa4
seohyun <tjgus5485@gmail.com>
2026-03-06 02:05:48 +0900

    chore: move SECRET_KEY to .env and update settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,14 +1,18 @@
 from pathlib import Path
 import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
+# 보안 향상, 코드 재사용, 환경 구분 가능
+env = environ.Env(DEBUG=(bool, False))
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-x)e3r)zdf5h0pa3wcrll8j1gx_xi26lvbm!kz6@@b_%lngtm-f"
+# 환경변수
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+
+# SECURITY를 .env로 이동하여 보호
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
